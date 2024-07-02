db4fb261a0c14b8bcf80dd0607616895c4a40538
Justin Lee <justinrhee1114@gmail.com>
2024-07-02 16:51:03 -0700

    added documentation for which lines to replace

--- lambda_functions/disable_rule.py
+++ lambda_functions/disable_rule.py
@@ -1,9 +1,24 @@
 import boto3
+import json
 
 def lambda_handler(event, context):
     client = boto3.client('events')
-    old_rule_name = 'old-event-pattern-rule'
-
+    s3_client = boto3.client('s3')
+    
+    # Replace with your S3 bucket name where the rule name is stored
+    bucket_name = 'justin-lambda-functions-bucket'  
+    # Name of the file where the rule name is saved in S3
+    rule_file = 'latest_rule.json'  
+    
+    # Retrieve the rule name from S3
+    try:
+        response = s3_client.get_object(Bucket=bucket_name, Key=rule_file)
+        rule_info = json.loads(response['Body'].read().decode('utf-8'))
+        old_rule_name = rule_info['rule_name']
+    except s3_client.exceptions.NoSuchKey:
+        return {"message": "No rule found to disable"}
+    
+    # Disable the old rule
     response = client.disable_rule(
         Name=old_rule_name
     )
--- lambda_functions/lambda_function.py
+++ lambda_functions/lambda_function.py
@@ -3,7 +3,15 @@ import boto3
 
 def lambda_handler(event, context):
     client = boto3.client('events')
-    rule_name = 'new-event-pattern-rule'
+    s3_client = boto3.client('s3')
+    
+    # Replace with your S3 bucket name where the rule name will be stored
+    bucket_name = 'justin-lambda-functions-bucket'  
+    # Name of the file where the rule name is saved in S3
+    rule_file = 'latest_rule.json'
+    
+    # Name of the new rule to be created - change this for different projects
+    rule_name = 'new-event-pattern-rule'  
     event_pattern = {
         "source": ["aws.codepipeline"],
         "detail-type": ["CodePipeline Pipeline Execution State Change"],
@@ -12,11 +20,19 @@ def lambda_handler(event, context):
         }
     }
 
+    # Create the EventBridge rule
     response = client.put_rule(
         Name=rule_name,
         EventPattern=json.dumps(event_pattern),
         State='ENABLED',
         Description='Rule triggered by CodePipeline state change'
+    )
+
+    # Save the rule name to S3
+    s3_client.put_object(
+        Bucket=bucket_name,
+        Key=rule_file,
+        Body=json.dumps({"rule_name": rule_name})
     )
 
     return response
